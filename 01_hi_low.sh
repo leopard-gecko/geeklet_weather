36ed720b5547bcebffddb56cd9de5f11ae6b10e0
leopard-gecko <kaz_bashi@yahoo.co.jp>
2019-02-05 08:29:49 +0900

    Update 01_hi_low.sh

--- 01_hi_low.sh
+++ 01_hi_low.sh
@@ -7,4 +7,4 @@ weather_url="https://www.accuweather.com/en/jp/koto-ku/221230/weather-forecast/2
 weather_today=`curl  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)' --silent ${weather_url/weather-forecast/daily-weather-forecast}"?day=1"`
 
 # 今日の最高温度と最低温度を取得して表示
-echo "$weather_today" | grep -A 6 'Today\|Tonight' | grep  -e 'large-temp\|small-temp\|temp-label tonight selected' | sed -e 's/<[^>]*>//g' | sed -e 's/&deg;/°/g' -e 's/^ *//' | tr "\r\n" " "
+echo "$weather_today" | grep -A 6 'Today\|Tonight' | grep  -e 'large-temp\|small-temp\|temp-label tonight selected' | sed -e 's/<[^>]*>//g' -e 's/&deg;/°/g' -e 's/^ *//' | tr "\r\n" " "
