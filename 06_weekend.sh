7598c1d71816d4138de7884ee8dc4c14040ae7d0
leopardgecko <kaz_bashi@yahoo.co.jp>
2019-09-12 16:46:44 +0900

    Update 06_weekend.sh

--- 06_weekend.sh
+++ 06_weekend.sh
@@ -30,12 +30,10 @@ echo "\033[0;${SUN_COLOR}mSunday\033[0m"
 printf "$sun_hi/$sun_lo "
 echo "$sun_data" | grep 'phrase' | awk -v "det=${detail}" -F\" '{print $det}' | sed -n 1p
 
-
-
 # 週末の天気アイコン取得して画像を保存（取得するアイコンのナンバーはゼロパディングする）
-icon_sat=`echo "$weather_weekend" | grep -e 'dailyForecast' | tr '{|}' '\n' | grep -B1 -m1 'Saturday' | sed -n 1p | sed -e 's/.*\"icon\":\([0-9]*\).*/\1/' | tr -cd '0123456789' | awk '{printf "%02d", $1}'`
+icon_sat=`echo "$sat_data" | grep -m1 'icon' | sed -e 's/.*\"icon\":\([0-9]*\).*/\1/' | tr -cd '0123456789' | awk '{printf "%02d", $1}'`
 icon_sat=`printf "%.2d\n" $icon_sat`
-icon_sun=`echo "$weather_weekend" | grep -e 'dailyForecast' | tr '{|}' '\n' | grep -B1 -m1 'Sunday' | sed -n 1p | sed -e 's/.*\"icon\":\([0-9]*\).*/\1/' | tr -cd '0123456789' | awk '{printf "%02d", $1}'`
+icon_sun=`echo "$sun_data" | grep -m1 'icon' | sed -e 's/.*\"icon\":\([0-9]*\).*/\1/' | tr -cd '0123456789' | awk '{printf "%02d", $1}'`
 icon_sun=`printf "%.2d\n" $icon_sun`
 
 echo "https://vortex.accuweather.com/adc2010/images/slate/icons/"$icon_sat"-l.png" | xargs curl --silent -o /tmp/weather_sat.png
