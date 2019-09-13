61ae83d5cb0f8dc40cff71dda83f0659b3271a8b
leopardgecko <kaz_bashi@yahoo.co.jp>
2019-09-13 16:29:19 +0900

    改訂

--- 02_minutecast.sh
+++ 02_minutecast.sh
@@ -7,4 +7,4 @@ weather_url="https://www.accuweather.com/en/jp/koto-ku/221230/weather-forecast/2
 weather_data=`curl -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)' --silent $weather_url`
 
 # MINUTECASTを取得して表示
-echo "$weather_data" | grep -e 'minuteCastSummary' | awk -F\" '{print $4}' | grep -v -e 'No precipitation' -e 'temporarily unavailable'
+echo "$weather_data" | grep 'minuteCastSummary' | cut -d"{" -f2 | cut -d"}" -f1 | tr ',' '\n' | tr -d '"' | grep 'phrase' | awk -F: '{print $2}' | grep -v -e 'No precipitation' -e 'temporarily unavailable'
--- 03_0to7.sh
+++ 03_0to7.sh
@@ -11,32 +11,31 @@ PM_COLOR=34
 later=0
 
 # 元データ取得
-my_hour=`date +%H`
-curl_data1=`curl  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)' --silent ${weather_url/weather-forecast/hourly-weather-forecast}`
-curl_data2=`curl  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)' --silent ${weather_url/weather-forecast/hourly-weather-forecast}"?day=1"`
-curl_data=$(echo "$curl_data1" "$curl_data2"| grep -e 'hourlyForecast' | tr '{|}' '\n' | grep 'localTime' | sed -n $(expr 1 + $later),$(expr 8 + $later)p)
+weather_data1=`curl -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)' --silent ${weather_url/weather-forecast/hourly-weather-forecast}`
+weather_data2=`curl -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X)' --silent ${weather_url/weather-forecast/hourly-weather-forecast}"?day=1"`
+hour_data=$(echo "$weather_data1" "$weather_data2"| grep 'hourlyForecast' | tr '{|}' '\n' | grep 'localTime' | sed -n $(expr 1 + $later),$(expr 8 + $later)p)
 
 # ８時間分の時刻を取得（後で配列変数として使う。以下同様）
 _IFS="$IFS";IFS="
 "
-current_time=(`echo "$curl_data" | awk -F\" '{print $10}'`) 
+current_time=(`echo "$hour_data" | awk -F'"localTime":' '{print $2}' | cut -d"\"" -f2 | cut -d"\"" -f1`) 
 IFS="$_IFS"
 
 # ８時間分の天気を取得
 _IFS="$IFS";IFS="
 "
-current_weather=(`echo "$curl_data" | awk -F\" '{print $14}'`) 
+current_weather=(`echo "$hour_data" | awk -F'"phrase":' '{print $2}' | cut -d"\"" -f2 | cut -d"\"" -f1`) 
 IFS="$_IFS"
 
-# ８時間分の天気アイコンのナンバーを取得
-current_icon=(`echo "$curl_data" | sed -e 's/.*\"icon\":\([0-9]*\).*/\1/' | tr -cd '0123456789\n' | awk '{printf "%02d\n", $1}'`)
+# ８時間分の天気アイコンのナンバーを取得してゼロパディング
+current_icon=(`echo "$hour_data" | awk -F'"icon":' '{print $2}' | cut -d"," -f1 | awk '{printf "%02d\n", $1}'`)
 
 # 時刻を左揃え14桁で表示
 _IFS="$IFS";IFS="
 "
 printf "%-14s" ${current_time[*]} | sed -e s/AM/`echo "\033[0;${AM_COLOR}mAM\033[0m"`/g -e s/PM/`echo "\033[0;${PM_COLOR}mPM\033[0m"`/g
 IFS="$_IFS"
-echo
+
 
 # 天気を左揃え14桁かつ２段で表示
 for (( i=0; i < ${#current_weather[@]}; ++i))
@@ -49,9 +48,8 @@ do
 echo "${current_weather[$i]}" | awk '{printf "%-14s", $2}'
 done
 
-# 天気アイコンを取得して保存（取得するアイコンのナンバーはゼロパディングする）
+# 天気アイコンを取得して保存
 for (( i=0; i < ${#current_icon[@]}; ++i))
 do
-current_icon[$i]=`printf "%02d" ${current_icon[$i]}`
 echo "https://vortex.accuweather.com/adc2010/images/slate/icons/${current_icon[$i]}-s.png" | xargs curl --silent -o /tmp/weather_hour_`expr $i + $later`.png
 done
